82dee9ff9dcb46fab451e4283e0439f51275cdec
jacob-wilkens <60865097+jacob-wilkens@users.noreply.github.com>
2022-02-04 17:19:00 -0600

    Create MST_improved.py

---
+++ MST_improved.py
@@ -0,0 +1,49 @@
+import csv
+
+class Graph:
+    def __init__(self):
+        pass
+
+    def readFromCSV(self, fileName):
+        data = list(csv.reader(open(fileName)))
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                data[i][j] = int(data[i][j])
+
+        self.data = data
+        self.size = len(self.data)
+    
+    def read(self, v): #graph.read([])
+        self.data = v
+        self.size = len(v)
+
+    def findMinimum(self, E):
+        return sorted(E, key=lambda x: x[2])[0]
+
+    def process(self):
+        #array hold all vertices
+        T = [False] * self.size #[False, False, False, False]
+        #array of edges
+        L = [] # [vertexIdA, vertexIdB, weight]
+        E = []
+        #do the algorithm
+        T[0] = True
+        for i in range(1, self.size):
+            for j in range(self.size):
+                for k in range(j, self.size):
+                    if T[j] != T[k]:
+                        E.append([j, k, self.data[j][k]])
+            targetEdge = self.findMinimum(E)
+            L.append(targetEdge)
+            T[targetEdge[0]] = True
+            T[targetEdge[1]] = True
+            E = []
+                
+        length = 0
+        for ele in L:
+            length += ele[2]
+        print("Minimum Spanning Tree Length is:", length)
+
+g = Graph()
+g.readFromCSV('graph.csv')
+g.process()
