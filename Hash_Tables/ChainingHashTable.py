3a474a688d96aa39a81ca3e1489ef0163a86955d
jacob-wilkens <60865097+jacob-wilkens@users.noreply.github.com>
2022-03-09 21:00:03 -0600

    Create ChainingHashTable.py

---
+++ Hash_Tables/ChainingHashTable.py
@@ -0,0 +1,40 @@
+class ChainingHashTable:
+
+    def __init__(self):
+        self.size = 10
+        self.array = [[] for _ in range(self.size)]
+
+    def hash(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        key_exists = False
+        bucket = self.array[self.hash(key)]
+
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                key_exists = True
+                break
+
+        if key_exists:
+            bucket[i] = ((key, value))
+        else:
+            bucket.append((key, value))
+
+    def search(self, key):
+        for i, kv in enumerate(self.array[self.hash(key)]):
+            k, v = kv
+            if key == k:
+                return v
+
+    # Function to display hashtable
+    def display_hash(self):
+        for i in range(self.size):
+            print(i, end=" ")
+
+            for j in self.array[i]:
+                print("-->", end=" ")
+                print(j, end=" ")
+
+            print()
